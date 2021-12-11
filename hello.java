4c1aa24d570c6f6a41a3c23e8d5d318377102f87
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2021-12-11 17:44:46 +0530

    update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 
-public class Hello {
+public class hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
