3508188ea6b0a5b73009db4d8ad2d865a2dfce2d
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2021-12-15 13:50:52 +0530

    update  If Example

--- hello.java
+++ hello.java
@@ -1,7 +1,10 @@
-
-public class hello {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}
+public class IfExample {  
+public static void main(String[] args) {  
+    //defining an 'age' variable  
+    int age=20;  
+    //checking the age  
+    if(age>18){  
+        System.out.print("Age is greater than 18");  
+    }  
+}  
+}  
